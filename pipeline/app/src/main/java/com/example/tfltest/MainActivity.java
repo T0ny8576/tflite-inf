2798ad3e115f7817b29b408a05abd23d5e66bbd7
T0ny8576 <qifeid@andrew.cmu.edu>
2022-10-10 20:37:49 -0400

    Create baseline method for power measurement

--- pipeline/app/src/main/java/com/example/tfltest/MainActivity.java
+++ pipeline/app/src/main/java/com/example/tfltest/MainActivity.java
@@ -1,53 +1,57 @@
 package com.example.tfltest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.RectF;
+import android.os.BatteryManager;
 import android.os.Bundle;
-
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Environment;
-import android.os.SystemClock;
-import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.tfltest.databinding.ActivityMainBinding;
+import com.google.mediapipe.formats.proto.LandmarkProto;
+import com.google.mediapipe.solutions.hands.HandLandmark;
+import com.google.mediapipe.solutions.hands.Hands;
+import com.google.mediapipe.solutions.hands.HandsOptions;
+import com.google.mediapipe.solutions.hands.HandsResult;
 
+import android.os.Environment;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
-import org.tensorflow.lite.support.image.ImageProcessor;
-import org.tensorflow.lite.support.image.TensorImage;
-import org.tensorflow.lite.task.core.BaseOptions;
-import org.tensorflow.lite.support.image.ops.Rot90Op;
-import org.tensorflow.lite.task.vision.classifier.Classifications;
-import org.tensorflow.lite.task.vision.classifier.ImageClassifier;
-import org.tensorflow.lite.task.vision.detector.Detection;
-import org.tensorflow.lite.task.vision.detector.ObjectDetector;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final String LOGFILE = "TFLTest.txt";
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
     private ExecutorService pool;
+    private BatteryManager mBatteryManager;
+    private BroadcastReceiver batteryReceiver;
+    private FileWriter logFileWriter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,11 +75,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        File logFile = new File(getExternalFilesDir(null), LOGFILE);
+        logFile.delete();
+        logFile = new File(getExternalFilesDir(null), LOGFILE);
+        Log.d(TAG, logFile.getPath());
+        try {
+            logFileWriter = new FileWriter(logFile, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        batteryReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, Integer.MIN_VALUE);
+//                Log.w(TAG, "Battery voltage = " + voltage);
+                String voltageMsg = TAG + ": Time: " + SystemClock.uptimeMillis() +
+                        "\tBattery voltage = " + voltage + "\n";
+                try {
+                    logFileWriter.write(voltageMsg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        registerReceiver(batteryReceiver, intentFilter);
+
+        mBatteryManager = (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
+
         pool = Executors.newFixedThreadPool(1);
-        pool.execute(() -> {
-            Pipeline pipeline = new Pipeline();
-            pipeline.runTest();
-        });
+        pool.execute(this::runTest);
+//        pool.execute(() -> {
+//            Pipeline pipeline = new Pipeline();
+//            pipeline.runTest();
+//        });
     }
 
     @Override
@@ -105,5 +139,217 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private Hands hands;
+
+    public void runTest() {
+        final String testBasedir = Environment.getExternalStorageDirectory().getPath() +
+                "/test_images/hands";
+        File testImages = new File(testBasedir + "/unknown");
+        final int imageCount = testImages.list().length;
+        Log.i(TAG, "Total Images: " + imageCount);
+
+        AtomicInteger flowControlToken = new AtomicInteger(2);
+
+        AtomicInteger good = new AtomicInteger();
+        AtomicInteger bad = new AtomicInteger();
+        AtomicInteger thumbsUp = new AtomicInteger();
+        AtomicInteger processed = new AtomicInteger();
+        AtomicLong start = new AtomicLong(SystemClock.uptimeMillis());
+
+        HandsOptions handsOptions = HandsOptions.builder()
+                .setStaticImageMode(true)
+                .setMaxNumHands(2)
+                .setRunOnGpu(true)
+                .build();
+        hands = new Hands(this, handsOptions);
+
+        hands.setResultListener(
+                handsResult -> {
+                    int processedUpdated = processed.incrementAndGet();
+                    if (handsResult.multiHandLandmarks().isEmpty()) {
+                        bad.incrementAndGet();
+                    } else {
+                        good.incrementAndGet();
+                        if (detectThumbsUp(handsResult)) {
+                            thumbsUp.incrementAndGet();
+                        }
+                    }
+                    if (processedUpdated % 200 == 0 || processedUpdated == imageCount) {
+//                        long elapsed = SystemClock.uptimeMillis() - start.get();
+                        int current = mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW);
+//                        Log.w(TAG, "TimeDelta: " + elapsed + "\tGood: " + good.get() +
+//                                "\tBad: " + bad.get() + "\tThumbsUp: " + thumbsUp.get() +
+//                                "\tCurrent: " + current);
+                        String testMag = TAG + ": Time: " + SystemClock.uptimeMillis() +
+                                "\tGood: " + good.get() + "\tBad: " + bad.get() +
+                                "\tThumbsUp: " + thumbsUp.get() + "\tCurrent: " + current + "\n";
+                        try {
+                            logFileWriter.write(testMag);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        start.set(SystemClock.uptimeMillis());
+                    }
+                    flowControlToken.incrementAndGet();
+                });
+        hands.setErrorListener((message, e) -> Log.e(TAG, "MediaPipe Hands error:" + message));
+
+//        detectHands(testBasedir + "/2022-09-02-22-03-36-685853-none(bolt).jpg");
+//        Log.i(TAG, "Warmup finished.");
+
+        for (int i = 0; i < imageCount; i++) {
+            File imageFile = testImages.listFiles()[i];
+            while (flowControlToken.get() <= 0) {
+
+            }
+            detectHands(imageFile.getPath());
+            flowControlToken.decrementAndGet();
+        }
+        while (flowControlToken.get() < 2) {
+
+        }
+
+//        Log.w(TAG, "Now running the TFL test...");
+//        Pipeline pipeline = new Pipeline();
+//        pipeline.runTest();
+        unregisterReceiver(batteryReceiver);
+        try {
+            logFileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        runOnUiThread(() -> {
+            binding.fab.setVisibility(View.INVISIBLE);
+        });
+    }
+
+    private void detectHands(String imagePath) {
+        Bitmap image = BitmapFactory.decodeFile(imagePath);
+        if (image != null) {
+            hands.send(image);
+        }
+    }
+
+    private boolean detectThumbsUp(HandsResult result) {
+        HashMap<String, Object> handState = getHandState(result);
+        if ((Boolean)handState.get("thumb_open") &&
+                (Boolean)handState.get("index_finger_closed") &&
+                (Boolean)handState.get("middle_finger_closed") &&
+                (Boolean)handState.get("ring_finger_closed") &&
+                (Boolean)handState.get("pinky_finger_closed")) {
+            if ((Double)handState.get("orientation") > 120 ||
+                    (Double)handState.get("orientation") < -150 ||
+                    ((Double)handState.get("orientation") < 60 &&
+                            (Double)handState.get("orientation") > -30)) {
+                if (handState.get("thumb_orientation").equals("up") &&
+                        handState.get("finger_y_order").equals("up")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private double dist2D(double x1, double y1, double x2, double y2) {
+        return Math.hypot(Math.abs(y2 - y1), Math.abs(x2 - x1));
+    }
+
+    private HashMap<String, Object> getHandState(HandsResult result) {
+        int width = result.inputBitmap().getWidth();
+        int height = result.inputBitmap().getHeight();
+        List<LandmarkProto.NormalizedLandmark> handLandmark =
+                result.multiHandLandmarks().get(0).getLandmarkList();
+
+        HashMap<String, Object> handState = new HashMap<>();
+
+        double x0 = handLandmark.get(HandLandmark.WRIST).getX() * width;
+        double y0 = handLandmark.get(HandLandmark.WRIST).getY() * height;
+        double x9 = handLandmark.get(HandLandmark.MIDDLE_FINGER_MCP).getX() * width;
+        double y9 = handLandmark.get(HandLandmark.MIDDLE_FINGER_MCP).getY() * height;
+        handState.put("orientation", Math.toDegrees(Math.atan2(y0 - y9, x9 - x0)));
+
+        double x1 = handLandmark.get(HandLandmark.THUMB_CMC).getX() * width;
+        double y1 = handLandmark.get(HandLandmark.THUMB_CMC).getY() * height;
+        double x2 = handLandmark.get(HandLandmark.THUMB_MCP).getX() * width;
+        double y2 = handLandmark.get(HandLandmark.THUMB_MCP).getY() * height;
+        double x3 = handLandmark.get(HandLandmark.THUMB_IP).getX() * width;
+        double y3 = handLandmark.get(HandLandmark.THUMB_IP).getY() * height;
+        double x4 = handLandmark.get(HandLandmark.THUMB_TIP).getX() * width;
+        double y4 = handLandmark.get(HandLandmark.THUMB_TIP).getY() * height;
+        double d01 = dist2D(x0, y0, x1, y1);
+        double d02 = dist2D(x0, y0, x2, y2);
+        double d03 = dist2D(x0, y0, x3, y3);
+        double d04 = dist2D(x0, y0, x4, y4);
+        handState.put("thumb_open", d04 > d03 && d03 > d02 && d02 > d01);
+
+        double x6 = handLandmark.get(HandLandmark.INDEX_FINGER_PIP).getX() * width;
+        double y6 = handLandmark.get(HandLandmark.INDEX_FINGER_PIP).getY() * height;
+        double x7 = handLandmark.get(HandLandmark.INDEX_FINGER_DIP).getX() * width;
+        double y7 = handLandmark.get(HandLandmark.INDEX_FINGER_DIP).getY() * height;
+        double x8 = handLandmark.get(HandLandmark.INDEX_FINGER_TIP).getX() * width;
+        double y8 = handLandmark.get(HandLandmark.INDEX_FINGER_TIP).getY() * height;
+        double d06 = dist2D(x0, y0, x6, y6);
+        double d07 = dist2D(x0, y0, x7, y7);
+        double d08 = dist2D(x0, y0, x8, y8);
+        handState.put("index_finger_closed", d06 > d07 && d07 > d08);
+
+        double x10 = handLandmark.get(HandLandmark.MIDDLE_FINGER_PIP).getX() * width;
+        double y10 = handLandmark.get(HandLandmark.MIDDLE_FINGER_PIP).getY() * height;
+        double x11 = handLandmark.get(HandLandmark.MIDDLE_FINGER_DIP).getX() * width;
+        double y11 = handLandmark.get(HandLandmark.MIDDLE_FINGER_DIP).getY() * height;
+        double x12 = handLandmark.get(HandLandmark.MIDDLE_FINGER_TIP).getX() * width;
+        double y12 = handLandmark.get(HandLandmark.MIDDLE_FINGER_TIP).getY() * height;
+        double d010 = dist2D(x0, y0, x10, y10);
+        double d011 = dist2D(x0, y0, x11, y11);
+        double d012 = dist2D(x0, y0, x12, y12);
+        handState.put("middle_finger_closed", d010 > d011 && d011 > d012);
+
+        double x14 = handLandmark.get(HandLandmark.RING_FINGER_PIP).getX() * width;
+        double y14 = handLandmark.get(HandLandmark.RING_FINGER_PIP).getY() * height;
+        double x15 = handLandmark.get(HandLandmark.RING_FINGER_DIP).getX() * width;
+        double y15 = handLandmark.get(HandLandmark.RING_FINGER_DIP).getY() * height;
+        double x16 = handLandmark.get(HandLandmark.RING_FINGER_TIP).getX() * width;
+        double y16 = handLandmark.get(HandLandmark.RING_FINGER_TIP).getY() * height;
+        double d014 = dist2D(x0, y0, x14, y14);
+        double d015 = dist2D(x0, y0, x15, y15);
+        double d016 = dist2D(x0, y0, x16, y16);
+        handState.put("ring_finger_closed", d014 > d015 && d015 > d016);
+
+        double x18 = handLandmark.get(HandLandmark.PINKY_PIP).getX() * width;
+        double y18 = handLandmark.get(HandLandmark.PINKY_PIP).getY() * height;
+        double x19 = handLandmark.get(HandLandmark.PINKY_DIP).getX() * width;
+        double y19 = handLandmark.get(HandLandmark.PINKY_DIP).getY() * height;
+        double x20 = handLandmark.get(HandLandmark.PINKY_TIP).getX() * width;
+        double y20 = handLandmark.get(HandLandmark.PINKY_TIP).getY() * height;
+        double d018 = dist2D(x0, y0, x18, y18);
+        double d019 = dist2D(x0, y0, x19, y19);
+        double d020 = dist2D(x0, y0, x20, y20);
+        handState.put("pinky_finger_closed", d018 > d019 && d019 > d020);
+
+        Integer[] landmarkYOrder = new Integer[handLandmark.size()];
+        for (int i = 0; i < landmarkYOrder.length; i++) {
+            landmarkYOrder[i] = i;
+        }
+        // A brief implementation of argsort
+        Arrays.sort(landmarkYOrder, (i1, i2) -> Float.compare(handLandmark.get(i1).getY(), handLandmark.get(i2).getY()));
+        String thumbOrientation = "unknown";
+        if (landmarkYOrder[0] == HandLandmark.THUMB_TIP && landmarkYOrder[1] == HandLandmark.THUMB_IP) {
+            thumbOrientation = "up";
+        } else if (landmarkYOrder[landmarkYOrder.length - 1] == HandLandmark.THUMB_TIP && landmarkYOrder[landmarkYOrder.length - 2] == HandLandmark.THUMB_IP) {
+            thumbOrientation = "down";
+        }
+        handState.put("thumb_orientation", thumbOrientation);
+
+        String fingerYOrder = "unknown";
+        if (y4 < y6 && y6 < y10 && y10 < y14 && y14 < y18) {
+            fingerYOrder = "up";
+        } else if (y4 > y6 && y6 > y10 && y10 > y14 && y14 > y18) {
+            fingerYOrder = "down";
+        }
+        handState.put("finger_y_order", fingerYOrder);
+
+        return handState;
     }
 }
